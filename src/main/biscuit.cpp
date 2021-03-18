0bd7701a7cbe48c9bc76bc7bd9ce4b32403ced39
noob77777 <abhishekpal7@outlook.com>
2021-03-18 23:15:40 +0530

    Added: benchmark

--- src/main/biscuit.cpp
+++ src/main/biscuit.cpp
@@ -2,3 +2,10 @@
 using std::cout;
 
 void biscuit::Hello() { cout << "Hello from biscuit db.\n"; }
+
+int biscuit::Radd(int x, int y) {
+  for (int i = 0; i < y; i++) {
+    x++;
+  }
+  return x;
+}
